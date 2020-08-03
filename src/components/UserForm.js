eb87b247cd0346bd68f186809ca8f60350b6d9d2
rilwan <olawuyirilwan98@gmail.com>
2020-08-03 05:40:58 +0100

    initial commit

---
+++ src/components/UserForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class UserForm extends Component {
+    state = {
+        
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default UserForm
